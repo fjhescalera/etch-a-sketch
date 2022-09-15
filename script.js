d7793b1d56d5fe6d2dea78878785ef3d3232b629
Raptaki <demstole@gmail.com>
2022-09-14 22:30:14 -0500

    Add a reset button to clear the grid from coloring

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 let container=document.querySelector(".container");
-console.log(container);
+
 function createDiv(numberDivs){
     for(let i=0;i<numberDivs;i++){
         let div=document.createElement("div");
         container.appendChild(div);
     }
 }
-createDiv(64);
+createDiv(100);
 let grid=container.querySelectorAll("div");
 function colorDiv(){
-this.style.backgroundColor="black";
-console.log(this);  
+    /*this.style.backgroundColor="black";*/ 
+    this.classList.add("on");
+    console.log(this)
 }
 grid.forEach(div=>div.addEventListener("pointerover",colorDiv.bind(div)));
+let button=document.querySelector(".reset");
+function resetDiv(){
+    grid.forEach(div=>div.classList.remove("on"));
+}
+button.addEventListener("click",resetDiv); /*when using () after a function we are calling it, not referencing it */
 
